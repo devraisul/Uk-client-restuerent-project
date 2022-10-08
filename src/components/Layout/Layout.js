5357e97d55907d8f80ab7629ef8827a642d442ad
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-10-09 00:00:15 +0600

    Fix add review not found problem

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -40,91 +40,22 @@ function Layout(props) {
       <>
         <Header history={props.history} />
         <Sidebar />
-        <div
-          className={classnames(classes.content, {
+        <div className={classnames(classes.content, {
             [classes.contentShift]: layoutState.isSidebarOpened,
-          })}
-        >
+          })} >
           <div className={classes.fakeToolbar} />
           <Switch>
             <Route path="/app/dashboard" component={Tables} />
             <Route path="/app/menu" component={MenuHandle} />
             <Route path="/app/add-dish/:menuName/:restaurentId/:menuId" component={DishHandle} />
-
-            {/* <Route path="/app/tables" component={Dashboard} /> */}
-            <Route path="/app/addReviewTag" component={Addveriation} />
+            <Route path="/app/addReview" component={Addveriation} />
             <Route path="/app/variation" component={Addveriation} />
             <Route path="/app/reviews" component={CustomerSatisfaction} />
             <Route path="/app/show-reviews" component={ShowReviews} />
             <Route path="/app/view-menu/:keyId/" component={ViewMenu} />
             <Route path="/app/changepassword" component={ChangePass} />
             <Route path="/app/updateRestaurent" component={UpdateRestaurent} />
-            {/* <Route path="/app/notifications" component={Notifications} /> */}
           </Switch>
-          {/* <Box
-            mt={5}
-            width={"100%"}
-            display={"flex"}
-            alignItems={"center"}
-            justifyContent="space-between" */}
-
-          {/* > */}
-          {/* <div>
-              <Link
-                color={'primary'}
-                href={'https://flatlogic.com/'}
-                target={'_blank'}
-                className={classes.link}
-              >
-                © 2022 Menu Qr Codes. All Rights Reserved.
-              </Link>
-            </div> */}
-          {/* <div>
-              © 2022 Menu Qr Codes. All Rights Reserved.
-            </div>
-            <div> */}
-          {/* <Link
-                href={'https://www.facebook.com/flatlogic'}
-                target={'_blank'}
-              >
-                <IconButton aria-label="facebook">
-                  <Icon
-                    path={FacebookIcon}
-                    size={1}
-                    color="#6E6E6E99"
-                  />
-                </IconButton>
-              </Link>
-              <Link
-                href={'https://twitter.com/flatlogic'}
-                target={'_blank'}
-              >
-                <IconButton aria-label="twitter">
-                  <Icon
-                    path={TwitterIcon}
-                    size={1}
-                    color="#6E6E6E99"
-                  />
-                </IconButton>
-              </Link>
-              <Link
-                href={'https://github.com/flatlogic'}
-                target={'_blank'}
-              >
-                <IconButton
-                  aria-label="github"
-                  style={{ marginRight: -12 }}
-                >
-                  <Icon
-                    path={GithubIcon}
-                    size={1}
-                    color="#6E6E6E99"
-                  />
-                </IconButton>
-              </Link> */}
-          {/* Order Booking App
-            </div>
-          </Box> */}
         </div>
       </>
     </div>
